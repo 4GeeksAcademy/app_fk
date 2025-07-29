46ab558af73268f9e8d2164aa989cc99a74a7e52
emilceinfante <emilceinfante@gmail.com>
2025-07-29 20:43:04 +0000

    app

--- src/app.py
+++ src/app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, render_template
 from pickle import load
 
 app = Flask(__name__)
-model = load(open("../models/decision_tree_classifier_default_42.sav", "rb"))
+model = load(open("../models/random_forest_regressor_default_42.sav", "rb"))
 class_dict = {
     "0": "Alumno no admitido",
     "1": "Alumno Admitido",
@@ -17,10 +17,9 @@ def index():
         val2 = float(request.form["val2"])
         val3 = float(request.form["val3"])
         val4 = float(request.form["val4"])
-        val5 = float(request.form["val5"])
-        val6 = float(request.form["val6"])
         
-        data = [[val1, val2, val3, val4, val5, val6]]
+        
+        data = [[val1, val2, val3, val4]]
         prediction = str(model.predict(data)[0])
         pred_class = class_dict[prediction]
     else:
