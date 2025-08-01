4bcba465823da7d74fd512ce30f1995f46b2e3a4
emilceinfante <emilceinfante@gmail.com>
2025-08-01 16:50:48 +0000

    app

--- src/app.py
+++ src/app.py
@@ -1,19 +1,10 @@
 from flask import Flask, request, render_template
 import os
-from joblib import load
-
-
-from flask import Flask, request, render_template
 from pickle import load
 
 app = Flask(__name__)
 model = load(open("../models/random_forest_regressor_default_42.sav", "rb"))
-        
-        # Ejemplo de uso:
-        # predictions = model.predict(X_new_data)
-  
-
-
+      
 @app.route("/", methods = ["GET", "POST"])
 def index():
     if request.method == "POST":
@@ -29,4 +20,5 @@ def index():
         #pred_class = class_dict[prediction]
     else:
         prediction = None
-    
+
+    return render_template("index.html", prediction = prediction)
